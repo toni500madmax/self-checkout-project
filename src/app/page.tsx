40d47cc799ed200dab3efdf607a07d020df9adfb
toni500madmax <toni500madmax@gmail.com>
2025-04-29 20:32:35 -0300

    `Update globals.css and page.tsx with new styles and components`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
+      <Button>enviar</Button>
+      <Input placeholder="Digite aqui" />
     </>
   );
 }
