047e5baf664fc38d358fb55bcf8063a0d353136f
toni500madmax <toni500madmax@gmail.com>
2025-04-30 10:57:33 -0300

    chore: adding prettier, eslint for auto save corrections in tailwindcss classes and imports and exports hierarchy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+/* const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+}); */
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -25,11 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
   );
 }
