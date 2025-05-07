e9eb2c4bfa687083c434da3f16166648b6262a0f
toni500madmax <toni500madmax@gmail.com>
2025-05-07 15:57:10 -0300

    feat: add product page and productheader component

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,38 @@
+
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductHeader from "./components/product-header";
+
+interface ProductPageProps {
+    params: Promise<{
+        slug: string;
+        productId: string;
+    }>
+}
+
+const ProductPage = async ({params}: ProductPageProps) => {
+    const { slug, productId } = await params;
+
+    const product = await db.product.findUnique({ where: { id: productId } });
+    
+    if(!product){
+        return notFound();
+    }
+
+    return (
+        <>
+            <ProductHeader product={product} />
+            <h1>Product Page</h1>
+            <p>
+            {slug}
+            </p>
+            <p>
+            {productId}
+            </p>
+        </>
+    );
+}
+ 
+export default ProductPage;
--- src/app/products/page.tsx
+++
@@ -1,9 +0,0 @@
-const ProductPage = () => {
-  return (
-    <>
-      <h1>Products</h1>
-    </>
-  );
-};
-
-export default ProductPage;
