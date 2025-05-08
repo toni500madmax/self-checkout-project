43094a68b4727fd51eb6b3b770692c821b3f9ad5
toni500madmax <toni500madmax@gmail.com>
2025-05-08 11:07:08 -0300

    feat: add product-details component, format currency function and add h-full to hetml and body tags in globals

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from './components/product.details';
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
@@ -15,22 +16,31 @@ interface ProductPageProps {
 const ProductPage = async ({params}: ProductPageProps) => {
     const { slug, productId } = await params;
 
-    const product = await db.product.findUnique({ where: { id: productId } });
+    const product = await db.product.findUnique({
+        where: { id: productId }, include: {
+            restaurant: {
+                select: {
+                    name: true,
+                    avatarImageUrl: true,
+                    slug: true
+            }
+        }
+    } });
     
     if(!product){
         return notFound();
     }
 
+    if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+        return notFound();
+    }
+
     return (
         <>
-            <ProductHeader product={product} />
-            <h1>Product Page</h1>
-            <p>
-            {slug}
-            </p>
-            <p>
-            {productId}
-            </p>
+            <div className="flex h-full flex-col">
+                <ProductHeader product={product} />
+                <ProductDetails product={product}/>
+            </div>
         </>
     );
 }
